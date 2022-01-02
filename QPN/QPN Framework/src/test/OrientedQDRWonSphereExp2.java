5bd9eff725fa2dc26a13f9050a27f28de1026882
DESKTOP-TU4OVNE\dahli <dahliajanabi@gmail.com>
2022-01-02 21:11:38 +0200

    Shpere exp2

--- QPN/QPN Framework/src/test/OrientedQDRWonSphereExp2.java
+++ QPN/QPN Framework/src/test/OrientedQDRWonSphereExp2.java
@@ -31,7 +31,7 @@ public class OrientedQDRWonSphereExp2 {
 	public static void main(String[] args) {
 
 		PetriNet pn = new PetriNet();
-		pn.PetriNetName = "Oriented QDRW on Sphere";
+		pn.PetriNetName = "Oriented QDRW on Sphere Exp2";
 		pn.NetworkPort = 1080;
 
 		DataComplexVector2D p0 = new DataComplexVector2D();
@@ -88,7 +88,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p00);
-		
+
 		DataComplexVector2D p01 = new DataComplexVector2D();
 		p01.SetName("p01");
 		p01.Value = new ComplexVector2D(Orientation.Right,
@@ -99,7 +99,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p01);
-		
+
 		DataComplexVector2D p02 = new DataComplexVector2D();
 		p02.SetName("p02");
 		p02.Value = new ComplexVector2D(Orientation.Right,
@@ -110,7 +110,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p02);
-		
+
 		DataComplexVector2D p03 = new DataComplexVector2D();
 		p03.SetName("p03");
 		p03.Value = new ComplexVector2D(Orientation.Right,
@@ -121,7 +121,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p03);
-		
+
 		DataComplexVector2D p10 = new DataComplexVector2D();
 		p10.SetName("p10");
 		p10.Value = new ComplexVector2D(Orientation.Right,
@@ -132,7 +132,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p10);
-		
+
 		DataComplexVector2D p11 = new DataComplexVector2D();
 		p11.SetName("p11");
 		p11.Value = new ComplexVector2D(Orientation.Right,
@@ -143,7 +143,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p11);
-		
+
 		DataComplexVector2D p12 = new DataComplexVector2D();
 		p12.SetName("p12");
 		p12.Value = new ComplexVector2D(Orientation.Right,
@@ -154,7 +154,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p12);
-		
+
 		DataComplexVector2D p13 = new DataComplexVector2D();
 		p13.SetName("p13");
 		p13.Value = new ComplexVector2D(Orientation.Right,
@@ -165,7 +165,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p13);
-		
+
 		DataComplexVector2D p20 = new DataComplexVector2D();
 		p20.SetName("p20");
 		p20.Value = new ComplexVector2D(Orientation.Right,
@@ -176,7 +176,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p20);
-		
+
 		DataComplexVector2D p21 = new DataComplexVector2D();
 		p21.SetName("p21");
 		p21.Value = new ComplexVector2D(Orientation.Right,
@@ -187,7 +187,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p21);
-		
+
 		DataComplexVector2D p22 = new DataComplexVector2D();
 		p22.SetName("p22");
 		p22.Value = new ComplexVector2D(Orientation.Right,
@@ -198,7 +198,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p22);
-		
+
 		DataComplexVector2D p23 = new DataComplexVector2D();
 		p23.SetName("p23");
 		p23.Value = new ComplexVector2D(Orientation.Right,
@@ -209,7 +209,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p23);
-		
+
 		DataComplexVector2D p30 = new DataComplexVector2D();
 		p30.SetName("p30");
 		p30.Value = new ComplexVector2D(Orientation.Right,
@@ -220,7 +220,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p30);
-		
+
 		DataComplexVector2D p31 = new DataComplexVector2D();
 		p31.SetName("p31");
 		p31.Value = new ComplexVector2D(Orientation.Right,
@@ -231,7 +231,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p31);
-		
+
 		DataComplexVector2D p32 = new DataComplexVector2D();
 		p32.SetName("p32");
 		p32.Value = new ComplexVector2D(Orientation.Right,
@@ -242,7 +242,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p32);
-		
+
 		DataComplexVector2D p33 = new DataComplexVector2D();
 		p33.SetName("p33");
 		p33.Value = new ComplexVector2D(Orientation.Right,
@@ -253,8 +253,7 @@ public class OrientedQDRWonSphereExp2 {
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f),
 						new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f), new ComplexValue(0.0f, 0.0f)));
 		pn.PlaceList.add(p33);
-		
-		
+
 		DataDoubleDouble pv = new DataDoubleDouble();
 		pv.SetName("pv");
 		pv.Value = new DoubleDouble(0.0f, 0.0f);
@@ -266,12 +265,12 @@ public class OrientedQDRWonSphereExp2 {
 		t0.InputPlaceName.add("p0");
 
 		Condition T0Ct1 = new Condition(t0, "p0", TransitionCondition.NotNull);
-		ArrayList<String> t0Output= new ArrayList<String>();
+		ArrayList<String> t0Output = new ArrayList<String>();
 		t0Output.add("p00");
 		t0Output.add("p01");
 		t0Output.add("p02");
 		t0Output.add("p03");
-		
+
 		GuardMapping grdT0 = new GuardMapping();
 		grdT0.condition = T0Ct1;
 		grdT0.Activations.add(new Activation(t0, "p0", TransitionOperation.MultiplyByHalfMultiPlaces, t0Output,
@@ -287,10 +286,15 @@ public class OrientedQDRWonSphereExp2 {
 		t1.InputPlaceName.add("p1");
 
 		Condition T1Ct1 = new Condition(t1, "p1", TransitionCondition.NotNull);
+		ArrayList<String> t1Output = new ArrayList<String>();
+		t1Output.add("p10");
+		t1Output.add("p11");
+		t1Output.add("p12");
+		t1Output.add("p13");
 
 		GuardMapping grdT1 = new GuardMapping();
 		grdT1.condition = T1Ct1;
-		grdT1.Activations.add(new Activation(t1, "p1", TransitionOperation.MultiplyByHalf, "p4",
+		grdT1.Activations.add(new Activation(t1, "p1", TransitionOperation.MultiplyByHalf, t1Output,
 				new HalfParameters(new Sign(1, 1), new Sign(1, -1), new Sign(1, 1), new Sign(1, 1), 1)));
 
 		t1.GuardMappingList.add(grdT1);
@@ -303,10 +307,15 @@ public class OrientedQDRWonSphereExp2 {
 		t2.InputPlaceName.add("p2");
 
 		Condition T2Ct1 = new Condition(t2, "p2", TransitionCondition.NotNull);
+		ArrayList<String> t2Output = new ArrayList<String>();
+		t2Output.add("p20");
+		t2Output.add("p21");
+		t2Output.add("p22");
+		t2Output.add("p23");
 
 		GuardMapping grdT2 = new GuardMapping();
 		grdT2.condition = T2Ct1;
-		grdT2.Activations.add(new Activation(t2, "p2", TransitionOperation.MultiplyByHalf, "p4",
+		grdT2.Activations.add(new Activation(t2, "p2", TransitionOperation.MultiplyByHalf, t2Output,
 				new HalfParameters(new Sign(1, 1), new Sign(1, 1), new Sign(-1, 1), new Sign(1, 1), 2)));
 
 		t2.GuardMappingList.add(grdT2);
@@ -319,10 +328,15 @@ public class OrientedQDRWonSphereExp2 {
 		t3.InputPlaceName.add("p3");
 
 		Condition T3Ct1 = new Condition(t3, "p3", TransitionCondition.NotNull);
+		ArrayList<String> t3Output = new ArrayList<String>();
+		t3Output.add("p30");
+		t3Output.add("p31");
+		t3Output.add("p32");
+		t3Output.add("p33");
 
 		GuardMapping grdT3 = new GuardMapping();
 		grdT3.condition = T3Ct1;
-		grdT3.Activations.add(new Activation(t3, "p3", TransitionOperation.MultiplyByHalf, "p4",
+		grdT3.Activations.add(new Activation(t3, "p3", TransitionOperation.MultiplyByHalf, t3Output,
 				new HalfParameters(new Sign(1, 1), new Sign(1, 1), new Sign(1, 1), new Sign(1, -1), 3)));
 
 		t3.GuardMappingList.add(grdT3);
@@ -335,19 +349,25 @@ public class OrientedQDRWonSphereExp2 {
 		T0P.InputPlaceName.add("p4");
 		T0P.IsReversible = true;
 
-		Condition T0PCt1 = new Condition(T0P, "p4", TransitionCondition.NotNull);
+		Condition T0PCt1 = new Condition(T0P, "p00", TransitionCondition.NotNull);
+		Condition T0PCt2 = new Condition(T0P, "p10", TransitionCondition.NotNull);
+		Condition T0PCt3 = new Condition(T0P, "p20", TransitionCondition.NotNull);
+		Condition T0PCt4 = new Condition(T0P, "p30", TransitionCondition.NotNull);
+		T0PCt3.SetNextCondition(LogicConnector.AND, T0PCt4);
+		T0PCt2.SetNextCondition(LogicConnector.AND, T0PCt3);
+		T0PCt1.SetNextCondition(LogicConnector.AND, T0PCt2);
 
 		GuardMapping grdT0P = new GuardMapping();
 		grdT0P.condition = T0PCt1;
-		
-		ArrayList<String> T0PInput= new ArrayList<String>();
+
+		ArrayList<String> T0PInput = new ArrayList<String>();
 		T0PInput.add("p00");
 		T0PInput.add("p10");
 		T0PInput.add("p20");
 		T0PInput.add("p30");
 
-		grdT0P.Activations.add(new Activation(T0P, T0PInput, TransitionOperation.ComplexVector2DAdditionMultiPlaces, "p0",
-				new HalfParameters(0, new Shift(+1, 0))));
+		grdT0P.Activations.add(new Activation(T0P, T0PInput, TransitionOperation.ComplexVector2DAdditionMultiPlaces,
+				"p0", new HalfParameters(0, new Shift(+1, 0))));
 
 		T0P.GuardMappingList.add(grdT0P);
 
@@ -359,12 +379,24 @@ public class OrientedQDRWonSphereExp2 {
 		T1P.InputPlaceName.add("p4");
 		T1P.IsReversible = true;
 
-		Condition T1PCt1 = new Condition(T1P, "p4", TransitionCondition.NotNull);
+		Condition T1PCt1 = new Condition(T1P, "p01", TransitionCondition.NotNull);
+		Condition T1PCt2 = new Condition(T0P, "p11", TransitionCondition.NotNull);
+		Condition T1PCt3 = new Condition(T0P, "p21", TransitionCondition.NotNull);
+		Condition T1PCt4 = new Condition(T0P, "p31", TransitionCondition.NotNull);
+		T1PCt3.SetNextCondition(LogicConnector.AND, T1PCt4);
+		T1PCt2.SetNextCondition(LogicConnector.AND, T1PCt3);
+		T1PCt1.SetNextCondition(LogicConnector.AND, T1PCt2);
+
+		ArrayList<String> T1PInput = new ArrayList<String>();
+		T1PInput.add("p01");
+		T1PInput.add("p11");
+		T1PInput.add("p21");
+		T1PInput.add("p31");
 
 		GuardMapping grdT1P = new GuardMapping();
 		grdT1P.condition = T1PCt1;
-		grdT1P.Activations.add(new Activation(T1P, "p4", TransitionOperation.ComplexVector2DMatrixAddition, "p1",
-				new HalfParameters(1, new Shift(-1, 0))));
+		grdT1P.Activations.add(new Activation(T1P, T1PInput, TransitionOperation.ComplexVector2DAdditionMultiPlaces,
+				"p1", new HalfParameters(1, new Shift(-1, 0))));
 
 		T1P.GuardMappingList.add(grdT1P);
 
@@ -376,12 +408,24 @@ public class OrientedQDRWonSphereExp2 {
 		T2P.InputPlaceName.add("p4");
 		T2P.IsReversible = true;
 
-		Condition T2PCt1 = new Condition(T2P, "p4", TransitionCondition.NotNull);
+		Condition T2PCt1 = new Condition(T2P, "p02", TransitionCondition.NotNull);
+		Condition T2PCt2 = new Condition(T0P, "p12", TransitionCondition.NotNull);
+		Condition T2PCt3 = new Condition(T0P, "p22", TransitionCondition.NotNull);
+		Condition T2PCt4 = new Condition(T0P, "p32", TransitionCondition.NotNull);
+		T2PCt3.SetNextCondition(LogicConnector.AND, T2PCt4);
+		T2PCt2.SetNextCondition(LogicConnector.AND, T2PCt3);
+		T2PCt1.SetNextCondition(LogicConnector.AND, T2PCt2);
+
+		ArrayList<String> T2PInput = new ArrayList<String>();
+		T2PInput.add("p02");
+		T2PInput.add("p12");
+		T2PInput.add("p22");
+		T2PInput.add("p32");
 
 		GuardMapping grdT2P = new GuardMapping();
 		grdT2P.condition = T2PCt1;
-		grdT2P.Activations.add(new Activation(T2P, "p4", TransitionOperation.ComplexVector2DMatrixAddition, "p2",
-				new HalfParameters(2, new Shift(0, +1))));
+		grdT2P.Activations.add(new Activation(T2P, T2PInput, TransitionOperation.ComplexVector2DAdditionMultiPlaces,
+				"p2", new HalfParameters(2, new Shift(0, +1))));
 
 		T2P.GuardMappingList.add(grdT2P);
 
@@ -393,12 +437,24 @@ public class OrientedQDRWonSphereExp2 {
 		T3P.InputPlaceName.add("p4");
 		T3P.IsReversible = true;
 
-		Condition T3PCt1 = new Condition(T3P, "p4", TransitionCondition.NotNull);
+		Condition T3PCt1 = new Condition(T3P, "p03", TransitionCondition.NotNull);
+		Condition T3PCt2 = new Condition(T0P, "p13", TransitionCondition.NotNull);
+		Condition T3PCt3 = new Condition(T0P, "p23", TransitionCondition.NotNull);
+		Condition T3PCt4 = new Condition(T0P, "p33", TransitionCondition.NotNull);
+		T3PCt3.SetNextCondition(LogicConnector.AND, T3PCt4);
+		T3PCt2.SetNextCondition(LogicConnector.AND, T3PCt3);
+		T3PCt1.SetNextCondition(LogicConnector.AND, T3PCt2);
+
+		ArrayList<String> T3PInput = new ArrayList<String>();
+		T3PInput.add("p03");
+		T3PInput.add("p13");
+		T3PInput.add("p23");
+		T3PInput.add("p33");
 
 		GuardMapping grdT3P = new GuardMapping();
 		grdT3P.condition = T3PCt1;
-		grdT3P.Activations.add(new Activation(T3P, "p4", TransitionOperation.ComplexVector2DMatrixAddition, "p3",
-				new HalfParameters(3, new Shift(0, -1))));
+		grdT3P.Activations.add(new Activation(T3P, T3PInput, TransitionOperation.ComplexVector2DAdditionMultiPlaces,
+				"p3", new HalfParameters(3, new Shift(0, -1))));
 
 		T3P.GuardMappingList.add(grdT3P);
 
